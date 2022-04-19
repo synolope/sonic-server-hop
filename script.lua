8e79fee8e6aa9e4d11b3037d4ed6b30ba1dde3b7
synolope <98623199+synolope@users.noreply.github.com>
2022-04-18 23:05:13 -0400

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,7 +1,7 @@
 if not game:IsLoaded() then game.Loaded:Wait() end
 
 syn.queue_on_teleport([[
-	loadstring(game:HttpGet("",true))()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/synolope/sonic-server-hop/main/script.lua",true))()
 ]])
 
 wait(2)
@@ -18,9 +18,12 @@ end
 
 wait(2)
 
-local Servers = game.HttpService:JSONDecode(game:HttpGet("https://games.roblox.com/v1/games/9049840490/servers/Public?sortOrder=Asc&limit=100"))
+while true do
+	local Servers = game.HttpService:JSONDecode(game:HttpGet("https://games.roblox.com/v1/games/9049840490/servers/Public?sortOrder=Asc&limit=100"))
 for i,v in pairs(Servers.data) do
   if v.playing ~= v.maxPlayers then
       game:GetService('TeleportService'):TeleportToPlaceInstance(game.PlaceId, v.id)
   end
+end
+wait(1)
 end
